63cbe93498d39e3ff3a485ac7b4775ed374ef146
dot_dash <dotdashgames@gmail.com>
2023-04-30 01:25:57 -0400

    Removed all files to allow entire server to be uploaded to GitHub using Git LFS

--- entities/entities/uk_coin/cl_init.lua
+++
@@ -1,7 +0,0 @@
-include('shared.lua')
-
-function ENT:Draw()
-    self.BaseClass.Draw(self)
-    --self:DrawEntityOutline(0.2)
-    self:DrawModel()
-end
--- entities/includes/modules/cl_m9k_famas.lua
+++
@@ -1,9 +0,0 @@
-module("ClientVars", package.seeall)
-
-ClientVars.KillClipActive = false
-ClientVars.KillClipReady = false
-ClientVars.KillClipDelayTimer = 0
-
-ClientVars.RapidHitActive = false
-ClientVars.RapidHitStacks = 0
-ClientVars.RapidHitStacksDelayTimer = 0
